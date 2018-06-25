24db64f3a98dcb936e0641647696a55faf8e2fd4
Danilo Henrique <danilo.2500@gmail.com>
2018-06-25 17:29:51 -0400

    Fixed a bug that considered D a special character when typing password

--- Your Key Manager/Supporting Files/Util.swift
+++ Your Key Manager/Supporting Files/Util.swift
@@ -9,3 +9,30 @@
 import Foundation
 
 let not = (!)
+
+class Util {
+    static func isValid(password: String) -> Bool {
+      
+        let oneUperrcaseLetter = "(?=.*[a-z])"
+        let oneLowercaseLetter = "(?=.*[A-Z])"
+        let oneNumber = "(?=.*?[0-9])"
+        //let oneSpecialCharacter = "(?i)^([[a-z][^a-z0-9\\s\\(\\)\\[\\]\\{\\}\\\\^\\$\\|\\?\\*\\+\\.\\<\\>\\-\\=\\!\\_]]*)"
+        //let oneSpecialCharacter = "[.*[^A-Za-z0-9].*]"
+        let oneSpecialCharacter = "(?=.*[!\"#$%&'()*+,-./:;<=>?@\\[\\\\\\])"
+        let EightCharacters = "{8,}"
+    
+        
+        //let regex = "^" + oneLowercaseLetter + oneUperrcaseLetter + oneNumber + oneSpecialCharacter + EightCharacters
+        //let passwordPredicate = NSPredicate(format:"SELF MATCHES %@", regex)
+        
+        let passwordPredicate = NSPredicate(format:"SELF MATCHES %@", "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!\"#$%&'()*+,-./:;<=>?@\\[\\\\\\]^_`{|}~])[A-Za-z\\dd!\"#$%&'()*+,-./:;<=>?@\\[\\\\\\]^_`{|}~]{8,}")
+        
+        return passwordPredicate.evaluate(with: password)
+    }
+    
+    static func isValid(email: String) -> Bool {
+        let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", regex)
+        return emailPredicate.evaluate(with: email)
+    }
+}
